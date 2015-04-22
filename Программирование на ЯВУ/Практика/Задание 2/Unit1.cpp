83496d079cbb482072b4ce92ee18f5528235bfc9
Group2Project <dolod@bk.ru>
2015-04-22 20:23:06 +0400

    ...

--- Программирование на ЯВУ/Практика/Задание 2/Unit1.cpp
+++ Программирование на ЯВУ/Практика/Задание 2/Unit1.cpp
@@ -15,4 +15,67 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 {
 }
 //---------------------------------------------------------------------------
- 
+
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+        StringGrid1->Show();
+        StringGrid1->RowCount=CSpinEdit1->Value;
+        StringGrid1->ColCount=CSpinEdit2->Value;
+        GroupBox1->Hide();
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Button2Click(TObject *Sender)
+{
+        int **matrix;
+        int n = CSpinEdit1->Value;
+        int m = CSpinEdit2->Value;
+        matrix = new int *[n];
+        for(int i=0;i<n;i++)
+                matrix[i]=new int[m];
+        for(int i=0;i<n;i++)
+                for(int j=0;j<m;j++)
+                        matrix[i][j]=StrToInt(StringGrid1->Cells[j][i]);
+        int min_j;
+        int min_i;
+        bool test(true);
+        for(int i=0;i<n;i++)
+        {
+                int min = matrix[i][0];
+                min_j = 0;
+                min_i = 0;
+                for(int j=1;j<m;j++)
+                        if(matrix[i][j] < min)
+                        {
+                                min = matrix[i][j];
+                                min_j = j;
+                                min_i = i;
+                        }
+
+                for(int j=0;j<n;j++)
+                        if(matrix[i][min_j] < matrix[j][min_j])
+                        {
+                                test=false;
+                                break;
+                        }
+                if(test)
+                        break;
+        }
+        if(test)
+                ShowMessage("Седловая точка имеет координаты:"+IntToStr(min_i) +":"+IntToStr(min_j));
+
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Button3Click(TObject *Sender)
+{
+        GroupBox1->Show();
+        StringGrid1->Hide();
+        for(int i=0;i<CSpinEdit1->Value;i++)
+                for(int j=0;j<CSpinEdit2->Value;j++)
+                        StringGrid1->Cells[j][i] = "";
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Button4Click(TObject *Sender)
+{
+        Close();        
+}
+//---------------------------------------------------------------------------
--- Программирование на ЯВУ/Практика/Задание 2/Unit1.dfm
+++ Программирование на ЯВУ/Практика/Задание 2/Unit1.dfm
@@ -1,8 +1,8 @@
 object Form1: TForm1
-  Left = 192
-  Top = 124
-  Width = 696
-  Height = 480
+  Left = 179
+  Top = 156
+  Width = 514
+  Height = 350
   Caption = 'Form1'
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
@@ -13,12 +13,27 @@ object Form1: TForm1
   OldCreateOrder = False
   PixelsPerInch = 96
   TextHeight = 13
-  object Panel1: TPanel
+  object GroupBox1: TGroupBox
     Left = 24
     Top = 16
-    Width = 273
-    Height = 169
+    Width = 257
+    Height = 129
+    Caption = #1056#1072#1079#1084#1077#1088#1099' '#1084#1072#1090#1088#1080#1094#1099':'
     TabOrder = 0
+    object Label1: TLabel
+      Left = 24
+      Top = 32
+      Width = 39
+      Height = 13
+      Caption = #1057#1090#1088#1086#1082#1080':'
+    end
+    object Label2: TLabel
+      Left = 24
+      Top = 64
+      Width = 44
+      Height = 13
+      Caption = #1057#1090#1086#1083#1073#1094#1099
+    end
     object CSpinEdit1: TCSpinEdit
       Left = 80
       Top = 24
@@ -33,12 +48,54 @@ object Form1: TForm1
       Height = 22
       TabOrder = 1
     end
+    object Button1: TButton
+      Left = 80
+      Top = 88
+      Width = 75
+      Height = 25
+      Caption = #1042#1074#1086#1076
+      TabOrder = 2
+      OnClick = Button1Click
+    end
   end
-  object Panel2: TPanel
-    Left = 344
-    Top = 48
-    Width = 113
-    Height = 153
+  object StringGrid1: TStringGrid
+    Left = 24
+    Top = 160
+    Width = 273
+    Height = 89
+    ColCount = 4
+    FixedCols = 0
+    RowCount = 3
+    FixedRows = 0
+    Options = [goFixedVertLine, goFixedHorzLine, goVertLine, goHorzLine, goRangeSelect, goEditing, goTabs]
     TabOrder = 1
+    Visible = False
+  end
+  object Button2: TButton
+    Left = 304
+    Top = 24
+    Width = 75
+    Height = 25
+    Caption = #1042#1074#1086#1076
+    TabOrder = 2
+    OnClick = Button2Click
+  end
+  object Button3: TButton
+    Left = 304
+    Top = 56
+    Width = 75
+    Height = 25
+    Caption = #1057#1073#1088#1086#1089
+    TabOrder = 3
+    OnClick = Button3Click
+  end
+  object Button4: TButton
+    Left = 304
+    Top = 88
+    Width = 75
+    Height = 25
+    Caption = #1042#1099#1093#1086#1076
+    TabOrder = 4
+    OnClick = Button4Click
   end
 end
